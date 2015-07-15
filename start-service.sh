62f25d6716353aa8b301a165bdacf6b4811923d8
Martin Hecher <martin.hecher@fraunhofer.at>
2015-07-15 11:46:05 +0200

    release(all): gatekeeper v0.7.0 serving WorkbenchUI at http://localhost/ and API at http://localhost/api/v0.7

--- start-service.sh
+++ start-service.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-docker rm -f api-gatekeeper
-docker run --name api-gatekeeper -d -p 8080:80 local/api-gatekeeper
+docker rm -f api-gatekeeper-v0.7.0
+
+docker run -d -p 80:80 --name api-gatekeeper-v0.7.0 \
+        --link workbench-ui-v0.7.0:workbench-ui-v0.7.0 \
+        --link microservice-sessions-v0.7.0:microservice-sessions-v0.7.0 \
+        --link microservice-metadata-extraction-v0.7.0:microservice-metadata-extraction-v0.7.0 \
+        --link microservice-sda-v0.7.0:microservice-sda-v0.7.0 \
+        --link microservice-sipgenerator-v0.7.0:microservice-sipgenerator-v0.7.0 \
+        local/api-gatekeeper
